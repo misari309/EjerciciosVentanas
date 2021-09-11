7a88e999d5c2335ad7b15420d982d358092428ad
misari309 <misari309@gmail.com>
2021-09-11 14:38:10 -0500

    Se agrega todo el documento

---
+++ src/ejerciciosventanas/EjerciciosVentanas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosventanas;
+
+/**
+ *
+ * @author camil
+ */
+public class EjerciciosVentanas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal obj1 = new Principal();
+        obj1.setVisible(true);
+    }
+    
+}
